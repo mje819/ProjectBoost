27693d5ac28f86352f095db4f14ef8bbde73a87e
mje819 <2260211@flourbluffschools.org>
2026-01-12 12:41:09 -0600

    add game audio

--- player/player.gd
+++ player/player.gd
@@ -4,6 +4,8 @@ class_name Player
 @export_range(750, 2500) var thrust := 1000.0
 @export var torque_thrust := 100.0
 
+@onready var rocket_audio: AudioStreamPlayer3D = $RocketAudio
+
 var transitioning := false
 
 func _ready() -> void:
@@ -13,6 +15,10 @@ func _process(delta: float) -> void:
 	if not transitioning:
 		if Input.is_action_pressed("boost"):
 			apply_central_force(basis.y * delta * thrust)
+			if not rocket_audio.is_playing():
+				rocket_audio.play()
+		else:
+			rocket_audio.stop()
 		
 		if Input.is_action_pressed("rotate_left"):
 			apply_torque(Vector3(0, 0, delta * torque_thrust))
@@ -22,12 +28,14 @@ func _process(delta: float) -> void:
 
 func crash_sequence() -> void:
 	transitioning = true
+	$ExplosionAudio.play()
 	print("KABOOM")
-	await get_tree().create_timer(1).timeout
+	await get_tree().create_timer(2.5).timeout
 	get_tree().reload_current_scene.call_deferred()
 
 func level_complete(next_level_path) -> void:
 	transitioning = true
+	$SuccessAudio.play()
 	await get_tree().create_timer(1).timeout
 	get_tree().change_scene_to_file.call_deferred(next_level_path)
 
