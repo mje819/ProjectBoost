1e0ca39ccbbe6b22c2d0b9174894e4c16de1c6d2
mje819 <2260211@flourbluffschools.org>
2026-01-12 11:46:19 -0600

    initial commit

---
+++ player/player.gd
@@ -0,0 +1,38 @@
+extends RigidBody3D
+class_name Player
+
+@export_range(750, 2500) var thrust := 1000.0
+@export var torque_thrust := 100.0
+
+func _ready() -> void:
+	pass
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("boost"):
+		apply_central_force(basis.y * delta * thrust)
+	
+	if Input.is_action_pressed("rotate_left"):
+		apply_torque(Vector3(0, 0, delta * torque_thrust))
+	
+	if Input.is_action_pressed("rotate_right"):
+		apply_torque(Vector3(0, 0, -delta * torque_thrust))
+
+func crash_sequence() -> void:
+	print("KABOOM")
+	await get_tree().create_timer(2.5).timeout
+	get_tree().reload_current_scene.call_deferred()
+	
+func level_complete(next_level_path) -> void:
+	await get_tree().create_timer(2.5).timeout
+	get_tree().change_scene_to_file.call_deferred(next_level_path)
+
+
+
+func _on_body_entered(body: Node) -> void:
+	if "Goal" in body.get_groups():
+		print("you win")
+		if body.file_path:
+			level_complete(body.file_path)
+	
+	if "Hazard" in body.get_groups():
+		crash_sequence()
